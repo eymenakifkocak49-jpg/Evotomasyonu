b33c6d333f95f607b3215272e27bc827a0a9a403
eymenakifkocak49-jpg <eymenakifkocak49@gmail.com>
2026-04-27 23:57:28 +0300

    404 hatasi fix

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ import os
 
 app = Flask(__name__)
 
-# Global değişken
+# Global veri deposu
 ev_verileri = {"sicaklik": 0, "nem": 0, "basinc": 0}
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Sistem Aktif."
 
 @app.route('/guncelle', methods=['POST'])
 def guncelle():
-    global ev_verileri  # BU SATIR EN ÖNEMLİSİ!
+    global ev_verileri
     data = request.get_json(force=True)
     if data:
         ev_verileri["sicaklik"] = data.get("sicaklik", 0)
         ev_verileri["nem"] = data.get("nem", 0)
         ev_verileri["basinc"] = data.get("basinc", 0)
-        print(f"Guncellendi: {ev_verileri}") # Loglarda bunu görmelisin
         return jsonify({"mesaj": "OK"}), 200
-    return jsonify({"mesaj": "Veri yok"}), 400
+    return jsonify({"mesaj": "Hata"}), 400
 
 @app.route('/durum', methods=['GET'])
 def durum():
@@ -25,4 +28,3 @@ def durum():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
-    
